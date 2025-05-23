e0796c5734470817d81f1bc4d535322c2ebba91c
lotcarnage <lotcarnage@gmail.com>
2025-05-24 02:27:25 +0900

    - [change] dequeueメソッドでは要素を捨てず、discardメソッドでデータを捨てる仕様に変更しました。

--- pypcmringbuffer/main.py
+++ pypcmringbuffer/main.py
@@ -36,15 +36,28 @@ class PyPcmRingQueue:
         return None
 
     def dequeue(self, size: int) -> numpy.ndarray:
+        if size < 0:
+            raise Exception("size must be positive")
         # データが無いなら例外を出す
         if len(self) < size:
             raise Exception("Queue is empty")
+        # 余計なコピーを避けてスライスでデータを返す
         # バッファ長を2倍で用意しているので取り出すデータは常に連続配置になっている
-        # スライスとコピーでデータを返す
         # ただし、headは常に0以上なので、headを基準にスライスする
         # 取り出すデータの長さはsizeで、headからsize分だけスライスする
-        # 取り出した後はheadをsize分だけ進める
         data = self.__queue[self.__head:self.__head + size]
+        return data
+
+    def discard(self, size: int) -> int:
+        # 実際に捨てた要素数を返す
+        # headをsize分だけ進めることでデータを捨てる
+        if size < 0:
+            raise Exception("size must be positive")
+        if len(self) <= size:
+            # 取り出すデータがキューの長さより大きい場合は、キューの長さを返す
+            size = len(self)
+            self.discard_all()
+            return size
         self.__head += size
         # headがqueue_lengthを超えたらheadとtailをバッファの1ブロック目に戻す
         if self.__buffer_length <= self.__head:
@@ -54,9 +67,9 @@ class PyPcmRingQueue:
             self.__queue[rewinded_head:rewinded_tail] = self.__queue[self.__head:self.__tail].copy()
             self.__head = rewinded_head
             self.__tail = rewinded_tail
-        return data
+        return size
 
-    def dispose(self):
+    def discard_all(self) -> None:
         self.__head = 0
         self.__tail = 0
         return None
--- test_basic.py
+++ test_basic.py
@@ -1,30 +1,39 @@
-import pytest
-
 from pypcmringbuffer import PyPcmRingQueue
 
 
 def test_init():
+
     queue_length = 10
     queue = PyPcmRingQueue(queue_length)
     assert queue is not None
     assert len(queue) == 0  # 初期状態ではキューは空であることを確認する
-    assert queue.remaining_capacity() == queue_length - len(queue)  # データ数操作後のキューの空き容量を確認する
+    assert queue.remaining_capacity() == queue_length
     queue.enqueue([0, 1, 2, 3, 4])
     assert len(queue) == 5  # データを追加した後のキューの長さを確認する
     assert queue.remaining_capacity() == queue_length - len(queue)  # データ数操作後のキューの空き容量を確認する
     queue.enqueue([5, 6, 7, 8, 9])
     assert len(queue) == 10  # データを追加した後のキューの長さを確認する
     assert queue.remaining_capacity() == queue_length - len(queue)  # データ数操作後のキューの空き容量を確認する
+    before_length = len(queue)
     pcm = queue.dequeue(3)
-    assert len(pcm) == 3  # データを取り出した後のキューの長さを確認する
-    assert queue.remaining_capacity() == queue_length - len(queue)  # データ数操作後のキューの空き容量を確認する
-    assert pcm[0] == 0  # 取り出したデータの内容を確認する
+    assert len(pcm) == 3  # 取り出したデータの長さを確認する
+    assert len(queue) == before_length  # dequeueでは要素を捨てない
+    # 取り出したデータの内容を確認する
+    assert pcm[0] == 0
     assert pcm[1] == 1
-    assert pcm[2] == 2  # 取り出したデータの内容を確認する
-    assert len(queue) == 7  # データを取り出した後のキューの長さを確認する
+    assert pcm[2] == 2
     pcm = queue.dequeue(3)
-    assert len(queue) == 4  # データを取り出した後のキューの長さを確認する
-    assert queue.remaining_capacity() == queue_length - len(queue)  # データ数操作後のキューの空き容量を確認する
+    assert len(pcm) == 3  # 取り出したデータの長さを確認する
+    assert len(queue) == before_length  # dequeueでは要素を捨てない
+    # 取り出したデータの内容を確認する
+    assert pcm[0] == 0
+    assert pcm[1] == 1
+    assert pcm[2] == 2
+    queue.discard(len(pcm))
+    assert len(queue) <= before_length  # discardで要素を捨てる
+    assert len(queue) == 7  # データを捨てた後のキューの長さを確認する
+    pcm = queue.dequeue(3)
+    queue.discard(len(pcm))
     assert pcm[0] == 3  # 取り出したデータの内容を確認する
     assert pcm[1] == 4
     assert pcm[2] == 5  # 取り出したデータの内容を確認する
@@ -32,6 +41,7 @@ def test_init():
     assert len(queue) == 10  # データを追加した後のキューの長さを確認する
     assert queue.remaining_capacity() == queue_length - len(queue)  # データ数操作後のキューの空き容量を確認する
     pcm = queue.dequeue(10)
+    queue.discard(len(pcm))
     assert len(queue) == 0  # データを取り出した後のキューの長さを確認する
     assert queue.remaining_capacity() == queue_length - len(queue)  # データ数操作後のキューの空き容量を確認する
     # 取り出したデータの内容を確認する
